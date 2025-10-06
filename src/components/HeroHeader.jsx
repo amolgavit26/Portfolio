c38d61c1ff47af7c873e2b976c5f1574452c4451
amolgavit26 <amolgavit158121@gmail.com>
2025-10-06 08:54:45 +0530

    Remove Particle

--- src/components/HeroHeader.jsx
+++ src/components/HeroHeader.jsx
@@ -2,57 +2,21 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import { SiX } from 'react-icons/si';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { TypeAnimation } from 'react-type-animation';
-
-import Particles, { initParticlesEngine } from '@tsparticles/react';
-import { loadSlim } from '@tsparticles/slim';
 
 import '../styles/HeroHeader.css';
 import ProfileImg from '../assets/Photo.png';
 
 function HeroHeader() {
-  const [engineReady, setEngineReady] = useState(false);
-
   useEffect(() => {
     AOS.init({ duration: 1000 });
-
-    initParticlesEngine(async (engine) => {
-      await loadSlim(engine); // Use loadFull if you install '@tsparticles/full'
-    }).then(() => {
-      setEngineReady(true);
-    });
   }, []);
 
   return (
     <section id="home" className="hero" data-aos="fade-up">
-      
-
-
-      
-      {/* {engineReady && (
-        <Particles
-          id="tsparticles"
-          options={{
-            fullScreen: { enable: false },
-            background: { color: 'transparent' },
-            particles: {
-              number: { value: 30 },
-              color: { value: '#00ffe7' },
-              size: { value: 2 },
-              move: { enable: true, speed: 0.6 },
-              opacity: { value: 0.3 },
-            },
-          }}
-        />
-      )} */}
-
-
-
-
-      
       <Container>
         <Row className="align-items-center">
           <Col md={7} className="hero-left">
@@ -80,16 +44,32 @@ function HeroHeader() {
             <div className="hero-contact-box">
               <p className="contact-label">Contact Me</p>
               <div className="social-icons">
-                <a href="https://www.linkedin.com/in/amolgavit26/" target="_blank" rel="noreferrer">
+                <a
+                  href="https://www.linkedin.com/in/amolgavit26/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <FaLinkedin />
                 </a>
-                <a href="https://x.com/amolgavit26" target="_blank" rel="noreferrer">
+                <a
+                  href="https://x.com/amolgavit26"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <SiX />
                 </a>
-                <a href="https://github.com/amolgavit26" target="_blank" rel="noreferrer">
+                <a
+                  href="https://github.com/amolgavit26"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <FaGithub />
                 </a>
-                <a href="https://www.instagram.com/amolgavit26/" target="_blank" rel="noreferrer">
+                <a
+                  href="https://www.instagram.com/amolgavit26/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <FaInstagram />
                 </a>
               </div>
